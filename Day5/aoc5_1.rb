500290b9a9aebc7867ace1bf72dd07bca2d8d5cb
Psilolouben <marky.rigas@gmail.com>
2022-12-05 12:42:03 +0200

    Day 5

---
+++ Day5/aoc5_1.rb
@@ -0,0 +1,18 @@
+fname = 'kokolala'
+input = File.read(fname)
+lines = input.split("\n")
+
+crate_map = ht = Hash.new {|h,k| h[k]=[]}
+lines.each do |l|
+  if l.include? '['
+    step = -3
+    (1..9).each { |i| step += 4; crate_map[i] += [l[step]] unless l[step].strip.empty? }
+  end
+
+  if l.include? 'move'
+    tim = l.split(' ')[1].to_i; from = l.split(' ')[3].to_i; to = l.split(' ')[5].to_i
+    tim.times { crate_map[to] += crate_map[from].shift(1); crate_map[to].rotate!(-1) }
+  end
+end
+
+crate_map.sort_by{|x, _| x}.to_h.map{|_,v| v[0]}.join
